d0e821dd41087317e8c9c136cd4c0e2e9615ffa3
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-08-29 15:40:55 -0300

    Dashboard responsive

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import styles from "@/styles/Admin.module.css";
+import Layout from "@/Components/Layout";
+import Link from "next/link";
+import { Titillium_Web } from "next/font/google";
+
+const titi = Titillium_Web({
+  weight: ["200", "300", "400", "600", "700", "900"],
+  subsets: ["latin"],
+});
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Layout>
+      <div className={styles.dashboard}>
+        <ul className={styles.breadcrumbs}>
+          <li>
+            <Link className={styles.breadcrumbLink} href={"/admin"}>
+              Admin
+            </Link>
+            &nbsp;/&nbsp;
+          </li>
+          <li>
+            <Link className={styles.breadcrumbLink} href={"/admin/dashboard"}>
+              Dashboard
+            </Link>
+          </li>
+        </ul>
+        <div className={styles.dashboardContainer}>
+          <Link href={"/admin/dashboard/drivers"} className={styles.dashboardDrivers}>
+            <p style={titi.style}>2023 Drivers Standings</p>
+            <img
+              src="https://www.transparentpng.com/thumb/formula-one/red-cars-formula-1-png-images-photo-6.png"
+              alt="helmet"
+            />
+          </Link>
+          <Link href={"/admin/dashboard/races"} className={styles.dashboardDrivers}>
+            <p style={titi.style}>2023 Calendar</p>
+            <img
+              src="https://www.transparentpng.com/thumb/formula-one/red-cars-formula-1-png-images-photo-6.png"
+              alt="helmet"
+            />
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
